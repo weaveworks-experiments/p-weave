5b708bfb631f199cffbf683da1e6dbaa3734619f
Luke Marsden <me@lukemarsden.net>
2017-06-08 19:20:23 +0100

    Automatically find the latest versions of things

--- Readme.md
+++ Readme.md
@@ -2,8 +2,10 @@
 
 ```
 git submodule update --recursive --init
-docker run -v $(pwd):/work -ti lmarsden/p-weave-builder
+docker run -v $(pwd):/work -ti lmarsden/p-weave-builder $TILE_VERSION_NUMBER
 ```
+
+Specify `$TILE_VERSION_NUMBER` as, for example, `0.0.1`.
 
 This builds a tile (`.pivotal` file), which is the artifact which you can upload to ops manager.
 
--- builder/build.sh
+++ builder/build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash -xe
+if [ "$1" -eq "" ]; then
+    echo "Please specify a version number (e.g. 0.0.1) as the first argument"
+fi
 
 # create releases
-bosh-cli create-release --dir releases/weave-scope-release --tarball releases/weave-scope.tgz releases/weave-scope-release/releases/weave-scope/weave-scope-0.0.12.yml
-bosh-cli create-release --dir releases/runtime-configurator-release --tarball releases/runtime-configurator.tgz releases/runtime-configurator-release/releases/runtime-configurator/runtime-configurator-0.0.1.yml
-bosh-cli create-release --dir releases/uaa-proxy-release --tarball releases/uaa-proxy.tgz releases/uaa-proxy-release/releases/uaa-proxy/uaa-proxy-0.0.2.yml
+
+latest_scope=$(ls releases/weave-scope-release/releases/weave-scope/ |sort --version-sort --field-separator=- --key=3,3 |tail -n 1)
+bosh-cli create-release --dir releases/weave-scope-release --tarball releases/weave-scope.tgz \
+    releases/weave-scope-release/releases/weave-scope/$latest_scope
+
+latest_runtime_configurator=$(ls releases/runtime-configurator-release/releases/runtime-configurator/ |sort --version-sort --field-separator=- --key=3,3 |tail -n 1)
+bosh-cli create-release --dir releases/runtime-configurator-release --tarball releases/runtime-configurator.tgz \
+    releases/runtime-configurator-release/releases/runtime-configurator/$latest_runtime_configurator
+
+latest_uaa_proxy=$(ls releases/uaa-proxy-release/releases/uaa-proxy/ |sort --version-sort --field-separator=- --key=3,3 |tail -n 1)
+bosh-cli create-release --dir releases/uaa-proxy-release --tarball releases/uaa-proxy.tgz \
+    releases/uaa-proxy-release/releases/uaa-proxy/$latest_uaa_proxy
 
 # download releases
 curl -sSL -o releases/cf-routing.tgz "https://bosh.io/d/github.com/cloudfoundry-incubator/cf-routing-release?v=0.156.0"
 curl -sSL -o releases/consul.tgz "https://bosh.io/d/github.com/cloudfoundry-incubator/consul-release?v=170"
 
 # create tile
-/usr/local/bundle/bin/hangar -n weave-cloud -r /work/releases -m /work/metadata/p-weave.yml -c /work/content_migrations/p-weave.yml -g /work/migrations -v 0.0.1 -s 3363
+/usr/local/bundle/bin/hangar -n weave-cloud -r /work/releases -m /work/metadata/p-weave.yml -c /work/content_migrations/p-weave.yml -g /work/migrations -v $1 -s 3363
